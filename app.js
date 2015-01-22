b2011adfedaf3985bf94dc8b07441efe8a11dadf
Adam Daigian <adam.daigian@gmail.com>
2015-01-21 22:27:14 -0800

    back to semi working place

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
+
 angular.module('eventHunt', [])
 
-// .config([ '$stateProvier', '$urlRouterProvider', function($stateProvier, $urlRouterProvider) {
-// 	$stateProvier(
-//     .state('home', {
-//       url: '/home',
-//       templateUrl: '/home.html',
-//       controller: 'MainCtrl'
-//     })
-//     .state('posts', {
-//       url: '/posts/{id}',
-//       templateUrl: '/posts.html',
-//       controller: 'PostsCtrl'
-//     });
+	.factory('posts', [function(){
+	  var o = {
+	    posts: []
+	  };
+	  return o;
+	}])
+	
+	.controller('MainCtrl', [ '$scope', function($scope){
+  	
+  	$scope.posts = [];
 
-// 	$urlRouterProvider.otherwise('home');
-// }])
-
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-
-.controller('MainCtrl', [ '$scope' function($scope) {
-	$scope.posts = []
 	$scope.addPost = function(){
-		if(!$scope.title || $scope.title === '') { return; }
-		$scope.posts.push({
-			title: $scope.title,
-			link: $scope.link,
-			upvotes: 0
-		});
-		$scope.title = '';
-		$scope.link = '';
+	  if(!$scope.title || $scope.title === '') { return; }
+	  $scope.posts.push({
+	    title: $scope.title,
+	    link: $scope.link,
+	    upvotes: 0
+	  });
+	  $scope.title = '';
+	  $scope.link = '';
 	};
-	$scope.incrementUpvotes = function(post) {
+
+	$scope.incrementalUpvotes = function(post) {
+		console.log('voting');
 		post.upvotes += 1;
 	};
-}]);
 
-.controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
-	$scope.posts.push({
-	  title: $scope.title,
-	  link: $scope.link,
-	  upvotes: 0,
-	  comments: [
-	    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-	    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-	  ]
-	});
-}]);
+
+
+
+	}]
+);
