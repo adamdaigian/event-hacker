10ea6b41f41ba9faec2949bfd83558aa7e748c4c
Adam Daigian <adam.daigian@gmail.com>
2015-01-21 21:53:44 -0800

    broken

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 angular.module('eventHunt', [])
-.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.posts = [
-  
-	];
 
+// .config([ '$stateProvier', '$urlRouterProvider', function($stateProvier, $urlRouterProvider) {
+// 	$stateProvier(
+//     .state('home', {
+//       url: '/home',
+//       templateUrl: '/home.html',
+//       controller: 'MainCtrl'
+//     })
+//     .state('posts', {
+//       url: '/posts/{id}',
+//       templateUrl: '/posts.html',
+//       controller: 'PostsCtrl'
+//     });
+
+// 	$urlRouterProvider.otherwise('home');
+// }])
+
+.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+.controller('MainCtrl', [ '$scope' function($scope) {
+	$scope.posts = []
 	$scope.addPost = function(){
-	  if(!$scope.title || $scope.title === '') { return; }
-	  $scope.posts.push({
-	    title: $scope.title,
-	    link: $scope.link,
-	    upvotes: 0
-	  });
-	  $scope.title = '';
-	  $scope.link = '';
+		if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		});
+		$scope.title = '';
+		$scope.link = '';
 	};
-
-	$scope.incrementalUpvotes = function(post) {
+	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
 	};
+}]);
 
-
-
-
-	}]
-);
+.controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+	$scope.posts.push({
+	  title: $scope.title,
+	  link: $scope.link,
+	  upvotes: 0,
+	  comments: [
+	    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+	    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+	  ]
+	});
+}]);
