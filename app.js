6fb3c118bceebe3439b1ec2342a181992d695d49
Adam Daigian <adam.daigian@gmail.com>
2015-01-22 09:27:13 -0800

    upvoting and downvoting working

--- app.js
+++ app.js
@@ -8,25 +8,33 @@ angular.module('eventHunt', [])
 	  return o;
 	}])
 	
-	.controller('MainCtrl', [ '$scope', function($scope){
+	.controller('MainCtrl', [
+		'$scope',
+		'posts',
+		function($scope, posts){
   	
-  	$scope.posts = [];
+  		$scope.posts = posts.posts;
 
-	$scope.addPost = function(){
-	  if(!$scope.title || $scope.title === '') { return; }
-	  $scope.posts.push({
-	    title: $scope.title,
-	    link: $scope.link,
-	    upvotes: 0
-	  });
-	  $scope.title = '';
-	  $scope.link = '';
-	};
+		$scope.addPost = function(){
+		  if(!$scope.title || $scope.title === '') { return; }
+		  $scope.posts.push({
+		    title: $scope.title,
+		    link: $scope.link,
+		    upvotes: 0
+		  });
+		  $scope.title = '';
+		  $scope.link = '';
+		};
 
-	$scope.incrementalUpvotes = function(post) {
-		console.log('voting');
-		post.upvotes += 1;
-	};
+		$scope.incrementalUpvotes = function(post) {
+			console.log('voting');
+			post.upvotes += 1;
+		};
+
+		$scope.incrementalDownvotes = function(post) {
+			console.log('voting');
+			post.upvotes -= 1;
+		};
 
 
 
